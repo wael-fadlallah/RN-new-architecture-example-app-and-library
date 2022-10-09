4f7c20b575bc7a70e0bb92e9240c3199172de4fa
Wael Fadlallah <waelf@mashreq.com>
2022-10-09 14:23:35 +0400

    init commit

---
+++ MyApp/App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {useState} from 'react';
+import {SafeAreaView, StatusBar, Text, Button} from 'react-native';
+import RTNCalculator from 'rtn-calculator/js/NativeCalculator';
+
+const App = () => {
+  const [result, setResult] = useState(null);
+  console.log({RTNCalculator});
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Text style={{marginLeft: 20, marginTop: 20}}>3+7={result ?? '??'}</Text>
+      <Button
+        title="Compute"
+        onPress={async () => {
+          const value = await RTNCalculator.add(3, 7);
+          setResult(value);
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+export default App;
